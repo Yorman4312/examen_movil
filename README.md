7505d0c70913e20339f60d7efe6b010ded2533dd
Yorman4312 <yormanalirio4312@gmail.com>
2026-03-20 06:32:32 -0500

    First commit - Carpetas

---
+++ README.md
@@ -0,0 +1,3 @@
+# examen_movil
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/examen_movil/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.examen_movil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
